fffdaa167bc00ef4e03f6ef6d0bf061fe61c0a09
Jonas Groß <jonas@mail.grossjonas.de>
2013-12-01 17:52:52 +0100

    Input on missing title

--- _posts/newPost.rb
+++ _posts/newPost.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
 require "time"
+require "readline"
 
 title = ARGV[0] if !(ARGV[0].nil? || ARGV[0].empty?)
 
 if !title.nil?
-	title=ARGV[0].gsub(/\ /, "-")
-	myTime=Time.new.iso8601.gsub(/T.*$/, "")
-
-	cmd="vim #{myTime}-#{title}.markdown"
-	#exec(cmd)
+	title=ARGV[0]
 else
-	puts "no arg"
+	title = Readline.readline("Title: ", true)
+	while title.empty?
+		title = Readline.readline("Title: ", true)
+	end	
 end
 
+title=title.gsub(/\ /, "-")
+myTime=Time.new.iso8601.gsub(/T.*$/, "")
 
-
+cmd="vim #{myTime}-#{title}.markdown"
+exec(cmd)
 
